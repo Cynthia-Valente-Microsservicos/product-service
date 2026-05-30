b3451b2e8a5b331322945b703374286a193b0d56
CYNahko <cynthiayasutake@gmail.com>
2026-05-29 21:18:55 -0300

    fixing productResource

--- src/main/java/store/product/ProductResource.java
+++ src/main/java/store/product/ProductResource.java
@@ -1,10 +1,11 @@
 package store.product;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,32 +18,32 @@ public class ProductResource implements ProductController {
     private ProductService productService;
     
     @Override
-    public ResponseEntity<List<ProductOut>> findAll() {
-        return ResponseEntity.ok(
-            ProductParser.to(
-                productService.findByAll()
-            )
-        );
+    public ResponseEntity<List<ProductOut>> findAll(@RequestParam(value = "name", required = false) String name) {
+        List<Product> products;
+        
+        if (name != null && !name.isBlank()) {
+            products = productService.findByNameLike(name);
+        } else {
+            products = productService.findByAll();
+        }
+        
+        return ResponseEntity.ok(ProductParser.to(products));
     }
 
     @Override
-    public ResponseEntity<ProductOut> findById(String id) {
+    public ResponseEntity<ProductOut> findById(@PathVariable("id") String id) {
         Product out = productService.findById(id);
         return out == null ?
             ResponseEntity.notFound().build() :
-            ResponseEntity.ok(
-                ProductParser.to(out)
-            );
+            ResponseEntity.ok(ProductParser.to(out));
     }
 
     @Override
-    public ResponseEntity<Void> create(ProductIn in, @RequestHeader("role") String role){
+    public ResponseEntity<Void> create(@RequestBody ProductIn in, @RequestHeader("role") String role) {
         if (role == null || !role.contains("ADMIN")) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
-        final Product p = productService.create(
-            ProductParser.to(in)
-        );
+        final Product p = productService.create(ProductParser.to(in));
         return ResponseEntity.created(
             ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -53,7 +54,7 @@ public class ProductResource implements ProductController {
     }
 
     @Override
-    public ResponseEntity<Void> delete(String id, @RequestHeader("role") String role){
+    public ResponseEntity<Void> delete(@PathVariable("id") String id, @RequestHeader("role") String role) {
         if (role == null || !role.contains("ADMIN")) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
         }
@@ -65,17 +66,4 @@ public class ProductResource implements ProductController {
     public ResponseEntity<Void> healthCheck() {
         return ResponseEntity.ok().build();
     }  
-    
-    @Override
-    public ResponseEntity<List<ProductOut>> findAllByName(@RequestParam(value = "name", required = false) String name) {
-        List<Product> products;
-        
-        if (name != null && !name.isBlank()) {
-            products = productService.findByNameLike(name);
-        } else {
-            products = productService.findByAll();
-        }
-        
-        return ResponseEntity.ok(ProductParser.to(products));
-    }
 }
